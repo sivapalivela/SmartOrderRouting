6fdd8f1ee64fb433bf5f7f41270ec8f29c823c1c
leelakrishnabharadwaj <bharat148vishnubhatla@gmail.com>
2020-06-13 13:38:17 +0530

    Spring Application

---
+++ SortSpringApplication/src/main/java/com/mthree/SortSpringApplication.java
@@ -0,0 +1,13 @@
+package com.mthree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SortSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SortSpringApplication.class, args);
+	}
+
+}
